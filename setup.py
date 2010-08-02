b16043755eb6f325c75e994961ccb56f761292c7
Ali Afshar <aafshar@gmail.com>
2010-08-02 13:33:15 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@
 Flask Principal
 ---------------
 
-Description goes here...
+Identity management for Flask.
 
 Links
 `````
 
 * `documentation <http://packages.python.org/Flask%20Principal>`_
+* `source <http://bitbucket.org/aafshar/flask-identity-main`_
 
 """
 from setuptools import setup
@@ -16,18 +17,18 @@ from setuptools import setup
 setup(
     name='Flask Principal',
     version='0.1',
-    url='<enter URL here>',
+    url='http://packages.python.org/Flask-Principal',
     license='MIT',
     author='Ali Afshar',
-    author_email='your-email-here@example.com',
-    description='<enter short description here>',
+    author_email='aafshar@gmail.com',
+    description='Identity management for flask',
     long_description=__doc__,
     packages=['flaskext'],
     namespace_packages=['flaskext'],
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'Flask'
+        'Flask', 'blinker'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
