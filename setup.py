c506caee91c0a6e5c75698697675e452ea20ce67
Ali Afshar <aafshar@gmail.com>
2010-09-12 11:42:04 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Principal',
-    version='0.1',
+    version='0.2',
     url='http://packages.python.org/Flask-Principal/',
     license='MIT',
     author='Ali Afshar',
