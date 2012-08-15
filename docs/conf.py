7d51981b90f5e48cf5bc11ec77615cbc644eaee4
Matt Wright <mdw1980@gmail.com>
2012-08-15 13:17:22 -0400

    Update to version 0.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -45,14 +45,14 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Flask Principal'
-copyright = u'2012, Matt Wright'
+copyright = u'2012, Ali Afshar'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '0.3'
+version = '0.3.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
